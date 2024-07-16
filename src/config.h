870d7c83cee2fa6de2b2f27cc68904b734bf604a
alialperucan <alialperucan@odakgis.com.tr>
2024-07-16 15:32:49 +0300

    Triangle Draw

---
+++ src/config.h
@@ -0,0 +1,14 @@
+#pragma once
+//iostream
+#include <iostream>
+//glad (will loading extra openGL functionaly)
+#include <glad/glad.h>
+//glfw3 
+#include <GLFW/glfw3.h>
+
+
+#include <fstream>
+#include <sstream>
+#include <string>
+
+#include <vector>
